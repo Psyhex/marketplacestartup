e306e1c605f678a3cd57398f1894630d463f1724
Psyhex <rokas.sapola@gmail.com>
2022-11-27 18:11:42 +0200

    added react router with pages, added mock data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import About from './pages/About';
+import { Route, Routes } from 'react-router-dom';
+import UserProfile from './pages/UserProfile';
+import Service from './pages/Service';
 
 function App() {
+  const home = "/";
+  const services = "/services";
+  const service = "/service/:id";
+  const userprofile = "/user/:id";
+  const about = "/about";
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path={home} element={<Home />} />
+        <Route path={services} element={<Services />} />
+        <Route path={about} element={<About />} />
+        <Route path={userprofile} element={<UserProfile />} />
+        <Route path={service} element={<Service />} />
+      </Routes>
     </div>
   );
 }
