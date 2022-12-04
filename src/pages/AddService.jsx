635730a38ddb64a2e406cefe42a41f5b2449d8d0
Psyhex <rokas.sapola@gmail.com>
2022-12-04 02:28:42 +0200

    Small changes

--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -1,45 +1,51 @@
 import React from "react";
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
 export default function AddService() {
   const minRowsTextField = 3;
   const maxRowsTextField = 6;
 
-  const inputTitle = "Skelbimo antraštė";
-  const inputDescription = "Aprašymas";
-  const inputPrice = "Kaina";
-  const inputLocation = "Vieta";
+  const inputTitle = "Service Title";
+  const inputDescription = "Description";
+  const inputPrice = "Price/day";
+  const inputLocation = "Location";
 
   return (
     <>
       <div className="AddService">
-        <TextField
-          className="inputTitle"
-          id="outlined-basic"
-          label={inputTitle}
-          variant="outlined"
-        />
-        <TextField
-          minRows={minRowsTextField}
-          maxRows={maxRowsTextField}
-          multiline={true}
-          className="inputDescrition"
-          id="outlined-basic"
-          label={inputDescription}
-          variant="outlined"
-        />
-        <TextField
-          className="inputTitle"
-          id="outlined-basic"
-          label={inputLocation}
-          variant="outlined"
-        />
-        <TextField
-          className="inputTitle"
-          id="outlined-basic"
-          label={inputPrice}
-          variant="outlined"
-        />
+        <Stack spacing={2} maxWidth="sm">
+          <TextField
+            className="inputTitle"
+            id="outlined-basic"
+            label={inputTitle}
+            variant="outlined"
+          />
+          <TextField
+            minRows={minRowsTextField}
+            maxRows={maxRowsTextField}
+            multiline={true}
+            className="inputDescrition"
+            id="outlined-basic"
+            label={inputDescription}
+            variant="outlined"
+          />
+          <TextField
+            className="inputTitle"
+            id="outlined-basic"
+            label={inputLocation}
+            variant="outlined"
+          />
+          <TextField
+            className="inputTitle"
+            id="outlined-basic"
+            label={inputPrice}
+            variant="outlined"
+          />
+          <Button variant="contained" className="submitButton">
+            Submit
+          </Button>
+        </Stack>
       </div>
     </>
   );
