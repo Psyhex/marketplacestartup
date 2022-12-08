3ae58538d148208776c896a1d813bf8e27ddef38
Psyhex <rokas.sapola@gmail.com>
2022-12-08 16:39:49 +0200

    fixed some errors in routes constants

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,8 @@
 export const HOME = "/";
 export const SERVICES = "/services";
 export const SERVICE = "/service/";
-export const USER_PROFILE = "/user/:id";
+export const USER_PROFILE = "/user/";
 export const ABOUT = "/about";
 export const REGISTER = "/register";
-export const LOGIN = "/register";
-export const ADD_SERVICE = "/addService";
+export const LOGIN = "/login";
+export const ADD_SERVICEl= "/addService";
