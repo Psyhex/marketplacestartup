a42484a9eb6498ee06382698a4be7787bd954fc4
Psyhex <rokas.sapola@gmail.com>
2022-12-13 19:06:45 +0200

    Service list sorting by default, asc, desc.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,32 @@
-import { Grid, Slider } from "@mui/material";
+import { Grid, Slider, Button } from "@mui/material";
 import React from "react";
+import { useState } from "react";
 import ServiceCard from "../components/ServiceCard/ServiceCard";
 import service10good from "../mockdata/service10good.json";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function Services() {
   const services = service10good;
+  const [data, setData] = useState(services);
   const priceRangeIndicator = "Price range filter";
+  const [sortType, setSortType] = useState("");
+  const defaultSort = "default";
+  const ascSort = "asc";
+  const descSort = "desc";
+
+  const handleChange = (event) => {
+    setSortType(event.target.value);
+    if (event.target.value === defaultSort) {
+      setData(services.sort((a, b) => a.service_id - b.service_id));
+    } else if (event.target.value === "asc") {
+      setData(services.sort((a, b) => a.service_price - b.service_price));
+    } else if (event.target.value === "desc") {
+      setData(services.sort((a, b) => b.service_price - a.service_price));
+    }
+  };
   return (
     <>
       <main>
@@ -18,11 +39,25 @@ function Services() {
                 defaultValue={30}
                 color="secondary"
               />
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Sort Type</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={sortType}
+                  label="Sort Type"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={defaultSort}>{defaultSort}</MenuItem>
+                  <MenuItem value={ascSort}>{ascSort}</MenuItem>
+                  <MenuItem value={descSort}>{descSort}</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
           </Grid>
           <Grid container item justify="center" spacing={3} xs={12} md={10}>
-            {services.map((service) => (
-              <Grid item key={services.service_id} xs={12} sm={6} md={4} lg={3}>
+            {data.map((service) => (
+              <Grid item key={data.service_id} xs={12} sm={6} md={4} lg={3}>
                 <ServiceCard service={service} />
               </Grid>
             ))}
